b6755eae5a070ac3e84fd4448763a5632c33a3ff
Riju-vv <riju.vv@progress.com>
2021-11-25 05:22:07 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# k8s_worker_node
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: k8s_worker_node
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
